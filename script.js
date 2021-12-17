73f2e5981f76b0a0fe156e03fe1b7eccfbd0ef0d
Darwin Audiomedia <67213583+darwinmedia@users.noreply.github.com>
2021-12-17 17:53:15 -0600

    C
    C

---
+++ script.js
@@ -0,0 +1,23 @@
+(function($){
+$(document).ready(function(){
+
+$('#cssmenu li.active').children('ul').show();
+$('#cssmenu li.has-sub>a').on('click', function(){
+  $(this).removeAttr('href');
+  var element = $(this).parent('li');
+  if (element.hasClass('active')) {
+    element.removeClass('active');
+    element.find('li').removeClass('active');
+    element.find('ul').slideUp();
+  } else {
+    element.addClass('active');
+    element.children('ul').slideDown();
+    element.siblings('li').children('ul').slideUp();
+    element.siblings('li').removeClass('active');
+    element.siblings('li').find('li').removeClass('active');
+    element.siblings('li').find('ul').slideUp();
+  }
+});
+
+});
+})(jQuery);
